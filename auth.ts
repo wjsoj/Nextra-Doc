903ecd9a105d35e490a0a900d0333a24fd25df87
wjsoj <wjs@wjsphy.top>
2024-09-23 21:26:03 +0800

    docs: :memo: add lobechat guide doc

--- auth.ts
+++ auth.ts
@@ -7,9 +7,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     clientId: process.env.AUTH_AUTHENTIK_CLIENT_ID,
     clientSecret: process.env.AUTH_AUTHENTIK_CLIENT_SECRET,
     issuer: process.env.AUTH_AUTHENTIK_ISSUER,
-    authorization: { params: { scope: 'openid email profile' } },
   })],
   secret: process.env.AUTH_SECRET,
   trustHost: true,
-  redirectProxyUrl: "https://docs.pku3d.com/api/auth",
 });
