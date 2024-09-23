09bc5fe1835ab20b7f7cadb9ea18464114c9a7f7
wjsoj <wjs@wjsphy.top>
2024-09-23 15:30:16 +0800

    fix: :bug: authentication bug

--- auth.ts
+++ auth.ts
@@ -3,8 +3,13 @@ import Authentik from "next-auth/providers/authentik";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [Authentik({
+    allowDangerousEmailAccountLinking: true,
     clientId: process.env.AUTH_AUTHENTIK_CLIENT_ID,
     clientSecret: process.env.AUTH_AUTHENTIK_CLIENT_SECRET,
     issuer: process.env.AUTH_AUTHENTIK_ISSUER,
-  })]
+    authorization: { params: { scope: 'openid email profile' } },
+  })],
+  secret: process.env.AUTH_SECRET,
+  trustHost: true,
+  redirectProxyUrl: "https://docs.pku3d.com/api/auth",
 });
